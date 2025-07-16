bd72cb1767b638c19f47899d9939c7994037d7b1
gajendra-ingle <gajendraingle01@gmail.com>
2025-07-17 02:18:30 +0530

    Add functionality to handle event requests and return structured responses

--- src/main/java/com/sprih/eventnotificationsystem/dto/EventRequest.java
+++ src/main/java/com/sprih/eventnotificationsystem/dto/EventRequest.java
@@ -1,5 +1,17 @@
 package com.sprih.eventnotificationsystem.dto;
 
-public class EventRequest {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+@Data
+public class EventRequest {
+	@NotBlank(message = "Event type is required")
+	private String eventType;
+
+	@NotNull(message = "Payload is required")
+	private Object payload;
+
+	@NotBlank(message = "Callback URL is required")
+	private String callbackUrl;
 }
