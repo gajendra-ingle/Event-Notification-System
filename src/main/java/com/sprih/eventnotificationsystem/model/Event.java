66f67fc915dd874f3c86b9b2c043542ccc81ad1d
gajendra-ingle <gajendraingle01@gmail.com>
2025-07-17 02:13:46 +0530

    Update event model to include Lombok annotations and field

--- src/main/java/com/sprih/eventnotificationsystem/model/Event.java
+++ src/main/java/com/sprih/eventnotificationsystem/model/Event.java
@@ -1,5 +1,14 @@
 package com.sprih.eventnotificationsystem.model;
 
-public class Event {
+import com.sprih.eventnotificationsystem.enums.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
+public class Event {
+	private String eventId;
+	private EventType eventType;
+	private Object payload;
+	private String callbackUrl;
 }
