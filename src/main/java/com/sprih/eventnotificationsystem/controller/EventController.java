eb21373222d00b11851f40ab76db55af9b86c030
gajendra-ingle <gajendraingle01@gmail.com>
2025-07-18 00:34:57 +0530

    Implemented EventController for handling event requests

--- src/main/java/com/sprih/eventnotificationsystem/controller/EventController.java
+++ src/main/java/com/sprih/eventnotificationsystem/controller/EventController.java
@@ -1,5 +1,35 @@
 package com.sprih.eventnotificationsystem.controller;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sprih.eventnotificationsystem.dto.EventRequest;
+import com.sprih.eventnotificationsystem.dto.EventResponse;
+import com.sprih.eventnotificationsystem.service.EventService;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/events")
 public class EventController {
 
+	@Autowired
+	private EventService eventService;
+
+	@PostMapping
+	public ResponseEntity<?> createEvent(@Valid @RequestBody EventRequest request) {
+		String eventId = UUID.randomUUID().toString();
+		log.info("Received new event: {}", eventId);
+		eventService.handleEvent(eventId, request);
+		return ResponseEntity.ok(new EventResponse(eventId, "Event accepted for processing"));
+
+	}
 }
