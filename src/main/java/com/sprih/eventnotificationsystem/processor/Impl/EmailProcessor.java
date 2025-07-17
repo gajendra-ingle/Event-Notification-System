0e8b4a9cbba269aebc8af5dd6425775089063e61
gajendra-ingle <gajendraingle01@gmail.com>
2025-07-18 00:49:09 +0530

    Implemented service layer for Email event handling

--- src/main/java/com/sprih/eventnotificationsystem/processor/Impl/EmailProcessor.java
+++ src/main/java/com/sprih/eventnotificationsystem/processor/Impl/EmailProcessor.java
@@ -1,5 +1,31 @@
 package com.sprih.eventnotificationsystem.processor.Impl;
 
-public class EmailProcessor {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import com.sprih.eventnotificationsystem.model.Event;
+import com.sprih.eventnotificationsystem.processor.EventProcessor;
+import com.sprih.eventnotificationsystem.util.CallbackNotifier;
 
+@Slf4j
+@Component
+public class EmailProcessor implements EventProcessor {
+
+	@Override
+	public void process(Event event) {
+		log.info("Processing EMAIL event: {}", event.getEventId());
+		try {
+			// Email processing delay of 5 seconds
+			Thread.sleep(5000);
+
+			// Random failure in 10% of the cases
+			if (Math.random() < 0.1) {
+				throw new RuntimeException("Simulated EMAIL failure");
+			}
+			log.info("EMAIL sent successfully for eventId: {}", event.getEventId());
+			CallbackNotifier.sendSuccess(event);
+		} catch (Exception e) {
+			log.error("EMAIL event failed: {}", event.getEventId(), e);
+			CallbackNotifier.sendFailure(event, e.getMessage());
+		}
+	}
 }
